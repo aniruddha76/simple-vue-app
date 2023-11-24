5ac991ed0e23c05e3061b87f974a2fe507d2ac23
aniruddha <aniruddhathakur76@gmail.com>
2023-11-24 21:51:00 +0530

    Vue App

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Hello from Aniruddha</h1>
+    <TodoList />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from "./components/TodoList.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    namw: "App",
+    components: {
+        TodoList
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body{
+        background-color: rgb(66, 66, 66);
+        color: aliceblue;
+    }
 </style>
